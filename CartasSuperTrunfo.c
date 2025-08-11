9443c20a952b0ff02ff98ff4cc9b2d9ba872b0ff
lohalzera <lohalzera22@gmail.com>
2025-08-11 04:59:10 +0000

    Inserindo primeira carta

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,17 +7,28 @@
 //Teste Fernando
 
 int main() {
-    printf ("Desafio supertrunfo lhl\n");
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
+    int populacao, pontosturisticos;
+    char estado;
+    char codigocarta[50];
+    char cidade[50];
+    float area;
+    float pib;
+
     
     // Cadastro das Cartas:
     // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
     // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
+    printf("Carta 1: \n");
+
+    printf("Estado: \n");
+    scanf("%c", &estado);
+
+    printf("Codigo da Carta: \n");
+    scanf("%s", &codigocarta);
     // Exibição dos Dados das Cartas:
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
-
     return 0;
 }
